8b200d548dbb5bfecdc8d7c9ed40101fbe7dabd1
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-13 17:15:08 -0400

    commit tree 76af7d806cc1c7b5d4f7f3f51befa532badac0ee

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|i| i.empty?}.length
   end
+
 end
